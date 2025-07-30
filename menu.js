3fa75c550ae6bfe28519aeb61890ce8a4167e0e0
AJITHKUMAR M <ajithkumar12204@gmail.com>
2025-07-30 18:37:04 +0530

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.querySelector('.menu-toggle');
+  const navList = document.querySelector('nav ul');
+  toggle.addEventListener('click', () => {
+    navList.classList.toggle('active');
+  });
+});
